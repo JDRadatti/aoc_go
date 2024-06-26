b25e52b314ea31d3c7e97dd9455874b5f00d7561
Justin Radatti <jdradatti@gmail.com>
2024-06-25 23:02:41 -0700

    Add main.go as the main runner for each day

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/JDRadatti/aoc_go/inputs"
+	"github.com/JDRadatti/aoc_go/internal/day01"
+	"log"
+	"os"
+)
+
+func run(day string) {
+	input, err := inputs.ReadInput(day)
+	if err != nil {
+		log.Fatalf("failed to read input for %s \n%v", day, err)
+	}
+
+	switch day {
+	default:
+		log.Fatal("unexpected day")
+	case "day01":
+		log.Printf("%s solution: %v", day, day01.SolutionA(input))
+	}
+}
+
+func help() {
+	os.Stdout.Write([]byte("\nmain.go - advent of code runner\n" +
+		"USAGE: go run main.go dayXX\n"))
+	os.Exit(0)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Printf("ERROR: invalid arguments.")
+		help()
+	}
+	dirname := os.Args[1]
+	run(dirname)
+}
