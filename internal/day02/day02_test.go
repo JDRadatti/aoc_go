db77d63453682b71fd8cc983ff7792a5825bca95
Justin Radatti <jdradatti@gmail.com>
2024-06-29 01:43:07 -0700

    Add day02_test.go

--- internal/day02/day02_test.go
+++ internal/day02/day02_test.go
@@ -35,3 +35,24 @@ Game 2: 1 blue, 2 green; 3 green, 15 blue, 1 red; 1 green, 1 blue
 		}
 	}
 }
+
+func TestDay02b(t *testing.T) {
+	inputs := [][]byte{
+        []byte(`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`),
+	}
+	expected := [...]int{
+		2286,
+	}
+	for i := 0; i < len(inputs); i++ {
+		result := SolutionB(inputs[i])
+
+		if expected[i] != result {
+			t.Errorf("Expected %d but got %d", expected[i], result)
+		}
+	}
+}
