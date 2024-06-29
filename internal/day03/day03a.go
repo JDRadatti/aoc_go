583b2a742dd13d7f002b2aab38803944339a57b2
Justin Radatti <jdradatti@gmail.com>
2024-06-29 02:08:04 -0700

    Add url to question description

--- internal/day03/day03a.go
+++ internal/day03/day03a.go
@@ -1,5 +1,6 @@
 package day03
 
+// https://adventofcode.com/2023/day/3
 func SolutionA(input []byte) int {
-    return 0
+	return 0
 }
--- internal/day03/day03b.go
+++ internal/day03/day03b.go
@@ -1,5 +1,6 @@
 package day03
 
+// https://adventofcode.com/2023/day/3
 func SolutionB(input []byte) int {
     return 0
 }
