4041425069a582e1c0d2bbe38e4b4e3cd883efb5
Justin Radatti <jdradatti@gmail.com>
2024-06-29 20:33:31 -0700

    Add test to day03_test.go

--- internal/day03/day03_test.go
+++ internal/day03/day03_test.go
@@ -4,10 +4,26 @@ import "testing"
 
 func TestDay03a(t *testing.T) {
 	inputs := [][]byte{
-		[]byte(``),
+		[]byte(`467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+`),
+		[]byte(`...
+1.1
+.*.
+1.1
+`),
 	}
 	expected := [...]int{
-		0,
+		4361,
+        4,
 	}
 	for i := 0; i < len(inputs); i++ {
 		result := SolutionA(inputs[i])
