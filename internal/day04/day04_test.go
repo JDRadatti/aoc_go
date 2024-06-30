20f5451d307df80e4ceb2c3ed4223258c52e7f50
Justin Radatti <jdradatti@gmail.com>
2024-06-30 01:15:59 -0700

    Update day04_test.go

--- internal/day04/day04_test.go
+++ internal/day04/day04_test.go
@@ -2,12 +2,28 @@ package day04
 
 import "testing"
 
-func TestDay03a(t *testing.T) {
+func TestDay04a(t *testing.T) {
 	inputs := [][]byte{
-		[]byte(``),
+		[]byte(`Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`),
+		[]byte(`Card 1: 41 | 53
+`),
+		[]byte(`Card 1: 1 2 3 4 5 6 7 8 9 | 1 2 3 4 5 6 7 8 9
+`),
+		[]byte(`Card 1: 1 2 | 3 4
+Card 2: 0 0 | 1 2
+`),
 	}
 	expected := [...]int{
-		4361,
+		13,
+		0,
+        256,
+        0,
 	}
 	for i := 0; i < len(inputs); i++ {
 		result := SolutionA(inputs[i])
@@ -18,12 +34,18 @@ func TestDay03a(t *testing.T) {
 	}
 }
 
-func TestDay03b(t *testing.T) {
+func TestDay04b(t *testing.T) {
 	inputs := [][]byte{
-		[]byte(``),
+		[]byte(`Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`),
 	}
 	expected := [...]int{
-        0,
+		30,
 	}
 	for i := 0; i < len(inputs); i++ {
 		result := SolutionB(inputs[i])
