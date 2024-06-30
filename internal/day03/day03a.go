bf39eafc8c0719b3d26d5908aa5eb16e97a99556
Justin Radatti <jdradatti@gmail.com>
2024-06-29 20:33:47 -0700

    Implement day03a

--- internal/day03/day03a.go
+++ internal/day03/day03a.go
@@ -1,6 +1,22 @@
 package day03
 
+import (
+    "github.com/JDRadatti/aoc_go/pkg/graphs"
+)
+
 // https://adventofcode.com/2023/day/3
-func SolutionA(input []byte) int {
-	return 0
+func SolutionA(schematic []byte) int {
+    // convert to graph where symbol is edge and number is node
+    // add nodes for each graph with an edge
+    // adjaceny matrix
+    // 
+    graph := graphs.Graph{}
+    graph.InitFromSchematic(schematic)
+
+
+    sum := 0
+    for n := range graph.Nodes {
+        sum += n.Value
+    }
+	return sum
 }
---
+++ pkg/graphs/graph.go
@@ -0,0 +1,112 @@
+package graphs
+
+import "bytes"
+
+type Graph struct {
+	Nodes map[Node]struct{}
+	Edges []Edge
+}
+
+type Node struct {
+	visited bool
+	Value   int
+	Id      int
+}
+
+type Edge struct {
+	neighbors map[Node]struct{}
+}
+
+func (edge *Edge) addNeighbor(node Node) {
+	if len(edge.neighbors) == 0 {
+		edge.neighbors = map[Node]struct{}{}
+	}
+	edge.neighbors[node] = struct{}{}
+}
+
+func (node *Node) updateID(value byte) {
+	node.Value = node.Value*10 + int(value) - 48
+}
+
+func (graph *Graph) addEdge(edge Edge) {
+	graph.Edges = append(graph.Edges, edge)
+}
+
+func (graph *Graph) addNode(node Node) {
+	if len(graph.Nodes) == 0 {
+		graph.Nodes = map[Node]struct{}{}
+	}
+	graph.Nodes[node] = struct{}{}
+}
+
+var directions [][]int = [][]int{
+	{1, 1},
+	{-1, -1},
+	{1, -1},
+	{-1, 1},
+	{1, 0},
+	{0, 1},
+	{-1, 0},
+	{0, -1},
+}
+
+func (graph *Graph) InitFromSchematic(schematic []byte) {
+	var split [][]byte = bytes.Split(schematic, []byte("\n"))
+	var nodes [][]*Node
+	var nilNode *Node = &Node{Value: 0}
+    id := 0
+
+	// First pass, init 2D array of Nodes
+	// Edge is a node with id = -1
+	for row := 0; row < len(split); row++ {
+		if len(split[row]) == 0 {
+			continue
+		}
+
+		nodes = append(nodes, []*Node{})
+        currNode := &Node{}
+		for col := 0; col < len(split[row]); col++ {
+			curr := split[row][col]
+			if curr >= 48 && curr <= 57 {
+				currNode.updateID(curr)
+                currNode.Id = id
+                id += 1
+				nodes[row] = append(nodes[row], currNode)
+			} else if curr == 46 { // curr byte is a "."
+				currNode = &Node{}
+				nodes[row] = append(nodes[row], nilNode)
+			} else { // other sympol / edge
+				currNode = &Node{}
+				nodes[row] = append(nodes[row], &Node{Value: -1})
+			}
+		}
+	}
+
+	for row := 0; row < len(nodes); row++ {
+		for col := 0; col < len(nodes[row]); col++ {
+
+			if nodes[row][col].Value >= 0 {
+				continue
+			}
+
+			edge := Edge{}
+			for dir := 0; dir < len(directions); dir++ {
+				x := directions[dir][0]
+				y := directions[dir][1]
+				if row+y >= 0 &&
+					col+x >= 0 &&
+					row+y < len(nodes) &&
+					col+x < len(nodes[row]) {
+					neighbor := nodes[row+y][col+x]
+					if neighbor.Value > 0 { // found node
+						edge.addNeighbor(*neighbor)
+						graph.addNode(*neighbor)
+					}
+				}
+			}
+			if len(edge.neighbors) > 0 {
+				graph.addEdge(edge)
+			}
+		}
+	}
+}
