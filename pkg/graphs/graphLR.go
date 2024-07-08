a6a113eec05a3b74f3209e8cb9eefde14d1cdc94
Justin Radatti <jdradatti@gmail.com>
2024-07-07 23:35:09 -0700

    Update graphLR

--- pkg/graphs/graphLR.go
+++ pkg/graphs/graphLR.go
@@ -2,35 +2,28 @@ package graphs
 
 import "regexp"
 
-const offset int = int('A')
-const numNodes int = 26 // ID contsrained to Alphabet
+type LRNode [2]string // LRNode[0] = left; LRNode[1] = right
 
-type LRNode [2]int // LRNode[0] = left; LRNode[1] = right
+type LRGraph map[string]LRNode
 
-type LRGraph [numNodes]LRNode
+func (g LRGraph) GetNode(id string) LRNode { return g[id] }
 
-func (g *LRGraph) GetNode(id []byte) LRNode { return g[g.Index(id)] }
-
-func (g *LRGraph) Index(id []byte) int {
-	return int(id[0]) - offset
-}
-
-func (g *LRGraph) AddNode(node []byte) bool {
+func (g LRGraph) AddNode(node []byte) bool {
 	re, _ := regexp.Compile("[A-Z]{3}")
 	nodes := re.FindAll(node, -1)
 	if len(nodes) == 3 {
-		node := g.Index(nodes[0])
-		left := g.Index(nodes[1])
-		right := g.Index(nodes[2])
-		g[node] = [2]int{left, right}
+		node := string(nodes[0])
+		left := string(nodes[1])
+		right := string(nodes[2])
+		g[node] = [2]string{left, right}
 		return true
 	}
 	return false
 }
 
-func (g *LRGraph) Search(instructions []int, start, end []byte) int {
+func (g LRGraph) Search(instructions []int, start, end string) int {
 	i, steps := 0, 0
-    currIndex, endIndex := g.Index(start), g.Index(end)
+    currIndex, endIndex := string(start), string(end)
 	for currIndex != endIndex {
 		currIndex = g[currIndex][instructions[i]]
 		i = (i + 1) % len(instructions)
