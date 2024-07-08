91f1def0ccd6030093b386894b6286e0237b1615
Justin Radatti <jdradatti@gmail.com>
2024-07-08 16:34:43 -0700

    Update graphLR to allow node matching using * char as substitute for any valid character.

--- pkg/graphs/graphLR.go
+++ pkg/graphs/graphLR.go
@@ -1,6 +1,13 @@
 package graphs
 
-import "regexp"
+import (
+    "regexp"
+    "strings"
+    "fmt"
+)
+
+const allowedChars string = "[A-Z1-9]"
+const allowedLength int = 3
 
 type LRNode [2]string // LRNode[0] = left; LRNode[1] = right
 
@@ -9,7 +16,7 @@ type LRGraph map[string]LRNode
 func (g LRGraph) GetNode(id string) LRNode { return g[id] }
 
 func (g LRGraph) AddNode(node []byte) bool {
-	re, _ := regexp.Compile("[A-Z]{3}")
+	re := regexp.MustCompile(fmt.Sprintf("%s{%d}", allowedChars, allowedLength))
 	nodes := re.FindAll(node, -1)
 	if len(nodes) == 3 {
 		node := string(nodes[0])
@@ -23,7 +30,7 @@ func (g LRGraph) AddNode(node []byte) bool {
 
 func (g LRGraph) Search(instructions []int, start, end string) int {
 	i, steps := 0, 0
-	for start != end {
+	for !g.NodeMatch(start, end) {
 		start = g.Next(instructions[i], start)
 		i = (i + 1) % len(instructions)
 		steps++
@@ -33,4 +40,12 @@ func (g LRGraph) Search(instructions []int, start, end string) int {
 
 func (g LRGraph) Next(instruction int, start string) string {
 	return g[start][instruction]
+}
+
+// NodeMatch determines if node N1 is the same as node N2
+// NodeMatch allows * characters in N2 to match with any allowed characters
+func (g LRGraph) NodeMatch(n1, n2 string) bool {
+    s := strings.ReplaceAll(n2, "*", allowedChars)
+    re, _ := regexp.Compile(s)
+    return re.MatchString(n1)
 }
